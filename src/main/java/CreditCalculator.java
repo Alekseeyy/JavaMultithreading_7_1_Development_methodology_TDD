58e65de1338b12752ea92e3e67284fa72ebc36c5
Aleksey Falev <aleksej.falev@mail.ru>
2021-12-14 17:46:35 +0300

    Realization

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -1,14 +1,15 @@
 public class CreditCalculator {
 
-    public int monthlyPayment(int sum, int period, int rate) {
-        return 0;
+    public int monthlyPayment(int sum, int period, double rate) {
+        return totalAmount(sum, period, rate) / period;
     }
 
-    public int totalAmount(int sum, int period, int rate) {
-        return 0;
+    public int totalAmount(int sum, int period, double rate) {
+        double percentages = (rate / 100) * (period / 12);
+        return (int) (sum * percentages) + sum;
     }
 
-    public int creditOverpayment(int sum, int period, int rate) {
-        return 0;
+    public int creditOverpayment(int sum, int period, double rate) {
+        return totalAmount(sum, period, rate) - sum;
     }
 }
--- src/test/java/CreditCalculatorTest.java
+++ src/test/java/CreditCalculatorTest.java
@@ -5,27 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class CreditCalculatorTest {
     private final int sum = 500_000;
     private final int period = 36;
-    private final int rate = 10;
+    private final double rate = 10;
 
     private CreditCalculator calculator = new CreditCalculator();
 
     @Test
     void monthlyPayment() {
-        int result = 20_000;
+        int result = 18_055;
         int actual = calculator.monthlyPayment(sum, period, rate);
         assertEquals(result, actual);
     }
 
     @Test
     void totalAmount() {
-        int result = 600_000;
+        int result = 650_000;
         int actual = calculator.totalAmount(sum, period, rate);
         assertEquals(result, actual);
     }
 
     @Test
     void creditOverpayment() {
-        int result = 100_000;
+        int result = 150_000;
         int actual = calculator.creditOverpayment(sum, period, rate);
         assertEquals(result, actual);
     }
